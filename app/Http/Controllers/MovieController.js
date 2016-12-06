ebaf16de3679cb35ecdbf40c7ba55f707e0b11ab
retimarcell <reti.marcell@gmail.com>
2016-12-06 10:53:39 +0100

    almost there!!!444!!!!!4!4!négy!!4

--- app/Http/Controllers/MovieController.js
+++ app/Http/Controllers/MovieController.js
@@ -1,28 +1,28 @@
 'use strict'
-
+ 
 const Database = use('Database')
 const Category = use('App/Model/Category')
 const Movie = use('App/Model/Movie')
 const Reserved = use('App/Model/Reserveds')
 const Validator = use('Validator')
 const fs = use('fs')
-
+ 
 class MovieController {
-
+ 
     * index(request, response) {
         const categories = yield Category.all()
-
+ 
         for (let category of categories) {
             const movies = yield category.movies().limit(3).fetch();
             category.topMovies = movies.toJSON();
         }
-
+ 
         yield response.sendView('main', {
             name: '',
             categories: categories.toJSON()
         })
     }
-
+ 
     * show(request, response) {
         const id = request.param('id');
         const movie = yield Movie.find(id);
@@ -31,26 +31,26 @@ class MovieController {
             movie: movie.toJSON()
         })
     }
-
+ 
     *createMovie(request,response) {
         const categories = yield Category.all()
         yield response.sendView('movieCreate', {
             categories: categories.toJSON()
         })
     }
-
+ 
     *doCreateMovie(request,response) {
         const movieData = request.except('_csrf');
-
+ 
         const rules = {
             name: 'required',
             description: 'required',
             hossz: 'required',
             category_id: 'required'
         };
-
+ 
         const validation = yield Validator.validateAll(movieData, rules)
-
+ 
         if (validation.fails()) {
             yield request
                 .withAll()
@@ -59,16 +59,16 @@ class MovieController {
             response.redirect('back')
             return
         }
-
+ 
         const movie = yield Movie.create(movieData)
         response.redirect('/')
     }
-
+ 
     *reserveMovie(request, response) {
         const movieID = request.param('id')
         const movie = yield Movie.find(movieID)
         const seats = yield Reserved.all()
-
+ 
         if (movie) {
             yield response.sendView('movieReserve', {
                 movie: movie.toJSON(),
@@ -76,24 +76,25 @@ class MovieController {
             })
         }
     }
-
+ 
     *doReserveMovie(request,response) {
         const reserveData = request.all()
         const movieID = request.param('id')
         const movie = yield Movie.find(movieID)
-
-        const seats = yield Seat.all()
-        const seatID = yield Seat.ids()
+ 
+        const seats = yield Reserved.all()
+        const seatID = yield Reserved.ids()
         var i;
-
+ 
         for (i = 0; i < seats.count; i++) {
             if (reserveData == 1) {
-                const tmp = yield Seat.find(ids[i]);
+                const tmp = yield Reserved.find(ids[i]);
                 tmp.taken = 1;
+                //mentés adatbázisba!!
             }
         }
         response.route('/')
     }
 }
-
+ 
 module.exports = MovieController
--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,26 +1,26 @@
 'use strict'
-
+ 
 const Validator = use('Validator')
 const User = use('App/Model/User')
 const Hash = use('Hash')
-
+ 
 class UserController {
     *login(request, response) {
         const logged = yield request.auth.check()
         if (logged) {
-            response.rediredt('/')
+            response.redirect('/')
         }
-
+ 
         yield response.sendView('login')
     }
-
+ 
     * doLogin(request, response) {
         const email = request.input('email')
         const password = request.input('password')
-
+ 
         try {
-            const login = yield request.auth.attempt(email, password)
-
+            yield request.auth.attempt(email, password)
+ 
             if (login) {
                 response.redirect('/')
                 return
@@ -37,46 +37,58 @@ class UserController {
                     ]
                 })
                 .flash()
-
-            response.redirect('/login')
+ 
+            response.redirect('login')
         }
     }
-
+ 
     *logout(request, response) {
         yield request.auth.logout()
         response.redirect('/')
     }
-
+ 
     * register(request, response) {
         const isLoggedIn = yield request.auth.check()
         if (isLoggedIn) {
-            response.redirect('/')
+            response.redirect('main')
         }
-
+ 
         yield response.sendView('register')
     }
-
+ 
     * doRegister(request, response) {
-        const registerData = request.except('_csrf');
-
-        const rules = {
-            username: 'required|alpha_numeric|unique:users',
-            email: 'required|email|unique:users',
-            password: 'required|min:4',
-            password_confirm: 'required|same:password',
-        };
-
-        const validation = yield Validator.validateAll(registerData, rules)
-
+        const userData = request.all()
+ 
+        const validation = yield Validator.validateAll(userData, {
+        username: 'required|alpha_numeric|unique:users',
+        email: 'required|email|unique:users',
+        password: 'required',
+        password_confirm: 'required|same:password'
+        })
+ 
         if (validation.fails()) {
-            yield request
-                .withAll()
-                .andWith({ errors: validation.messages() })
-                .flash()
-            response.redirect('back')
-            return
+        yield request
+            .withOut('password', 'password_confirm')
+            .andWith({ errors: validation.messages() })
+            .flash()
+ 
+        response.route('register')
+        return;
+       
         }
-    }
+       
+        const user = new User()
+        user.username = userData.username
+        user.email = userData.email
+        user.password = yield Hash.make(userData.password)
+ 
+        yield user.save()
+ 
+        yield request.auth.login(user)
+       
+        response.route('/')
 }
-
+ 
+}
+ 
 module.exports = UserController
